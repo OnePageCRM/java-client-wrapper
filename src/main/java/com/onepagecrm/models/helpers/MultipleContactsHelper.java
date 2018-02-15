697b478440af8570deb139e16b43f6c93bcfb440
Cillian Myles <mylsey4thewin@gmail.com>
2018-02-15 14:57:03 +0000

    AND-762 support multiple contacts;

---
+++ src/main/java/com/onepagecrm/models/helpers/MultipleContactsHelper.java
@@ -0,0 +1,32 @@
+package com.onepagecrm.models.helpers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Cillian Myles on 15/02/2018.
+ * Copyright (c) 2018 OnePageCRM. All rights reserved.
+ */
+@SuppressWarnings({"unused", "WeakerAccess", "Convert2Diamond"})
+public class MultipleContactsHelper {
+
+    public static String toString(List<String> contactIds) {
+        return contactIds == null ? "" : TextHelper.join(",", contactIds);
+    }
+
+    public static String toString(Map<String, Object> filterParams) {
+        return filterParams == null ? "" : TextHelper.join(",", filterParams.keySet());
+    }
+
+    public static List<String> toList(String contactIds) {
+        return TextHelper.isEmpty(contactIds) ? new ArrayList<String>() :
+                Arrays.asList(contactIds.split("\\s*,\\s*"));
+    }
+
+    public static List<String> toList(Map<String, Object> filterParams) {
+        return filterParams == null ? new ArrayList<String>() :
+                toList(TextHelper.join(",", filterParams.keySet()));
+    }
+}
