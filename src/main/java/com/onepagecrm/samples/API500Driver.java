6d2a4e35e63ef4986888c3ddbad687b23bc16ae8
Cillian Myles <mylsey4thewin@gmail.com>
2018-06-28 10:27:22 +0100

    API-500 test changes form saved actions project;

---
+++ src/main/java/com/onepagecrm/samples/API500Driver.java
@@ -0,0 +1,62 @@
+package com.onepagecrm.samples;
+
+import com.onepagecrm.OnePageCRM;
+import com.onepagecrm.exceptions.OnePageException;
+import com.onepagecrm.models.Action;
+import com.onepagecrm.models.User;
+import com.onepagecrm.models.internal.PredefinedAction;
+import com.onepagecrm.net.request.Request;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * @author Cillian Myles <cillian@onepagecrm.com> on 13/10/2017.
+ */
+public class API500Driver {
+
+    private static final Logger LOG = Logger.getLogger(API500Driver.class.getName());
+
+    public static void main(String[] args) throws OnePageException {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("config.properties");
+
+            // Load the properties file
+            prop.load(input);
+
+        } catch (IOException e) {
+            LOG.severe("Error loading the config.properties file");
+            LOG.severe(e.toString());
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    LOG.severe("Error closing the config.properties file");
+                    LOG.severe(e.toString());
+                }
+            }
+        }
+
+        OnePageCRM.setServer(Request.STAGING_SERVER);
+
+        User loggedInUser = User.login(
+                prop.getProperty("username"),
+                prop.getProperty("password"));
+
+        LOG.info("Logged in User: " + loggedInUser);
+
+        final List<PredefinedAction> actions = Action.listPredefined();
+        int i = 0;
+        for (PredefinedAction action : actions) {
+            LOG.info("[" + (i++) + "] - " + action.toString());
+        }
+    }
+}
