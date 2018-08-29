a07759b60d09d51eebe9a472a5e66cfa349f8655
fraggjkee <fraggjkee@gmail.com>
2018-08-29 11:51:07 +0300

    AND-946 Merge fix

--- src/main/java/com/onepagecrm/models/fabricators/DealItemFabricator.java
+++ src/main/java/com/onepagecrm/models/fabricators/DealItemFabricator.java
@@ -11,13 +11,13 @@ public class DealItemFabricator {
         return new DealItem()
                 .setName("iPhone")
                 .setDescription("Description")
-                .setUpdatedAt("123")
+                .setModifiedAt("123")
                 .setCreatedAt("456")
                 .setItemGroupId("j2-9-21j2j3j;d")
                 .setPredefinedItemId("predef item id")
                 .setDealId("-2-92jj;dj;wdajdaw")
-                .setAmount(10)
-                .setPrice(15000)
+                .setAmount(10.0)
+                .setPrice(15000.0)
                 .setId("5947a6749007ba40b8ecbb0djp");
     }
 }
